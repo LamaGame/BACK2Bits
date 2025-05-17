295d6056829080b9679c7739cafbd3453349b410
Leo G. <95503088+LamaGame@users.noreply.github.com>
2025-05-17 22:50:40 +0200

    Add files via upload

---
+++ products.js
@@ -0,0 +1,64 @@
+const categories = ["Nintendo", "Sega", "Atari", "Sony"];
+
+const products = {
+  "1": {
+    category: "Nintendo",
+    name: "Nintendo Entertainment System (NES)",
+    price: "89.99",
+    img: "https://placehold.net/400x400.png",
+  },
+  "2": {
+    category: "Nintendo",
+    name: "Super Nintendo Entertainment System (SNES)",
+    price: "99.99",
+    img: "https://placehold.net/400x400.png",
+  },
+  "3": {
+    category: "Sega",
+    name: "Sega Mega Drive",
+    price: "79.99",
+    img: "https://placehold.net/400x400.png",
+  },
+  "4": {
+    category: "Sega",
+    name: "Sega Saturn",
+    price: "129.99",
+    img: "https://placehold.net/400x400.png",
+  },
+  "5": {
+    category: "Atari",
+    name: "Atari 2600",
+    price: "69.99",
+    img: "https://placehold.net/400x400.png",
+  },
+  "6": {
+    category: "Atari",
+    name: "Atari Jaguar",
+    price: "149.99",
+    img: "https://placehold.net/400x400.png",
+  },
+  "7": {
+    category: "Sony",
+    name: "Sony PlayStation (PS1)",
+    price: "89.99",
+    img: "https://placehold.net/400x400.png",
+  },
+  "8": {
+    category: "Sony",
+    name: "Sony PlayStation 2",
+    price: "109.99",
+    img: "https://placehold.net/400x400.png",
+  },
+  "9": {
+    category: "Sony",
+    name: "Sony PlayStation Portable (PSP)",
+    price: "79.99",
+    img: "https://placehold.net/400x400.png",
+  },
+  "10": {
+    category: "Nintendo",
+    name: "Nintendo 64",
+    price: "109.99",
+    img: "https://placehold.net/400x400.png",
+  },
+};
